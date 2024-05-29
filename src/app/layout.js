2da281423aeaec5c36700e7526d62b61c237299a
bigyapti <bigyapti2000@gmail.com>
2024-05-29 07:48:53 +0545

    title update

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Portfolio Website',
+  title: 'Bigyapti Bashyal',
   description: 'Portfolio Website',
 }
 
